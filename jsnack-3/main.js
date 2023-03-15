cc5e800c59f2bdd87bf397bbe53d52fce7d0d535
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-03-15 11:49:10 +0100

    Snack 3 con ciclo while

--- jsnack-3/main.js
+++ jsnack-3/main.js
@@ -1,6 +1,6 @@
 /* Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
-let somma= 0
+/* let somma= 0
 let number;
 for( let i = 0; i < 10 ; i++) {
     console.log(i);
@@ -8,6 +8,20 @@ for( let i = 0; i < 10 ; i++) {
     if(number>0) {
         somma+=number;
     }
+}
+console.log(somma); */
+
+
+let somma= 0
+let number;
+let i = 0
+while (i < 10) {
+    console.log(i);
+    number= parseInt(prompt('inserisci un numero'));
+    if(number>0) {
+        somma+=number;
+    }
+    i++
 }
 console.log(somma);
 
